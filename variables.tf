cb75622007ef3c9fd24800842c9868f745689ced
Jawad Grar <jawad.grar@gmail.com>
2023-05-25 09:53:46 -0400

    add variables file

---
+++ variables.tf
@@ -0,0 +1,46 @@
+variable "project_name" {
+  description = "Name of the Google Cloud project"
+  type        = string
+  default     = "Taxio Dev v1"
+}
+
+# TODO: ensure the id is unique and available
+variable "project_id" {
+  description = "ID of the Google Cloud project"
+  type        = string
+}
+
+variable "billing_account" {
+  description = "Billing account for the Google Cloud project"
+  type        = string
+}
+
+variable "android_app_display_name" {
+  description = "Display name of the Firebase Android app"
+  type        = string
+}
+
+variable "android_app_package_name" {
+  description = "Package name of the Firebase Android app"
+  type        = string
+}
+
+variable "ios_app_display_name" {
+  description = "Display name of the Firebase iOS app"
+  type        = string
+}
+
+variable "ios_app_bundle_id" {
+  description = "Bundle ID of the Firebase iOS app"
+  type        = string
+}
+
+variable "web_app_display_name" {
+  description = "Display name of the Firebase Web app"
+  type        = string
+}
+
+variable "firebase_region_name" {
+  description = "Region for the Firebase project"
+  type        = string
+}
