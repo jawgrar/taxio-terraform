c80ac905fab415d7e93b8fed6618be38310150e9
Jawad <jawad.grar@gmail.com>
2023-05-25 20:17:08 -0400

    test

--- main.tf
+++ main.tf
@@ -15,17 +15,17 @@ provider "google-beta" {
 }
 
 
-# # Configures the provider to use the resource block's specified project for quota checks.
-# provider "google-beta" {
-#   user_project_override = true
-# }
+# Configures the provider to use the resource block's specified project for quota checks.
+provider "google-beta" {
+  user_project_override = true
+}
 
-# # Configures the provider to not use the resource block's specified project for quota checks.
-# # This provider should only be used during project creation and initializing services.
-# provider "google-beta" {
-#   alias                 = "no_user_project_override"
-#   user_project_override = false
-# }
+# Configures the provider to not use the resource block's specified project for quota checks.
+# This provider should only be used during project creation and initializing services.
+provider "google-beta" {
+  alias                 = "no_user_project_override"
+  user_project_override = false
+}
 
 # # Creates a new Google Cloud project.
 # resource "google_project" "default" {
